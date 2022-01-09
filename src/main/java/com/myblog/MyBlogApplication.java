9c1b29feeeccaf366ba7b23bfb75e7e0fe20c010
Robel <mrobel77@gmail.com>
2022-01-09 22:03:41 +0600

    al

--- src/main/java/com/myblog/MyBlogApplication.java
+++ src/main/java/com/myblog/MyBlogApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyBlogApplication extends SpringBootServletInitializer {
+public class MyBlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBlogApplication.class, args);
